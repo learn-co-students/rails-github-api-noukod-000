31606fb3f9e1b1d88ef92bf182575ee9141aa9a7
dowpm <mcdowpm@gmail.com>
2019-02-12 16:22:55 -0500

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,21 @@
 class RepositoriesController < ApplicationController
   
   def index
+    # binding.pry
+    # session.delete('token')
+    token = 'token ' + session[:token]
+    user = Faraday.get('https://api.github.com/user') do |req|
+      req.headers['Authorization'] = token
+      req.headers['Accept'] = 'application/json'
+    end
+
+    @user = JSON.parse user.body
+    repos = Faraday.get('https://api.github.com/user/repos') do |req|
+      req.headers['Authorization'] = token
+      req.headers['Accept'] = 'application/json'
+    end
+
+    @repo_info = JSON.parse repos.body
   end
 
 end
