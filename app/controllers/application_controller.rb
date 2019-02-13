eb1518c2fc52ffd2b9a10ab6141c50aada99cda5
dowpm <mcdowpm@gmail.com>
2019-02-12 22:00:33 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :authenticate_user, only: [:index]
+  before_action :authenticate_user
 
   private
 
   def authenticate_user
-    # session.delete('token')
+    session.delete('token')
     client_id = ENV['GITHUB_CLIENT_ID']
     state = ENV['GITHUB_STATE']
     github_url = "https://github.com/login/oauth/authorize?client_id=#{client_id}"
--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -18,4 +18,14 @@ class RepositoriesController < ApplicationController
     @repo_info = JSON.parse repos.body
   end
 
+  def create
+    response = Faraday.post 'https://api.github.com/user/repos' do |req|
+      req.body = { 'name': params[:name] }
+      req.headers['Authorization'] = 'token ' + session[:token]
+      req.headers['Accept'] = 'application/json'
+    end
+    
+    redirect_to root_path
+  end
+
 end
